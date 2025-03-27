3c6dc3b94e84f79931122b3ffc1391b2b675b619
Vishnu <vishnukaranth04@gmail.com>
2025-03-27 17:31:50 +0530

    27th March 2025

---
+++ StringFomatting.py
@@ -0,0 +1,4 @@
+name = input("enter a name: ")
+age = input("enter a age: ")
+height = input("enter a height: ")
+print("Name = {0}\nage = {1}\nheight = {2}\n".format(name, age, height))
---
+++ chekforcharandnum.py
@@ -0,0 +1,7 @@
+str = input("Enter a string: ")
+if(str.isalpha):
+    print("It contains only characters")
+if(str.isdigit()):
+    print("Str contains only digits")
+if(str.isalnum()):
+    print("Str contains both characters and digits")
