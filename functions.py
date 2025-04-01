9f963502e5bf0ec9654af76fc2541dedd6ea2c24
Vishnu <vishnukaranth04@gmail.com>
2025-04-01 17:18:53 +0530

    1st April 2025

---
+++ functions.py
@@ -0,0 +1,32 @@
+# a = 10
+# def fun1():
+#     print(a)
+#     b = 20
+#     print(b)
+
+# def fun2():
+#     print(a)
+#     c = 30
+#     print(c)
+# print(a)
+# fun1()
+# print(a)
+# fun2()
+# print(a)
+
+a = 10
+def fun1():
+    a = 100
+    b = 20
+    print(a)
+    print(b)
+def fun2():
+    a = 1000
+    c = 30
+    print(a)
+    print(c)
+print(a)
+fun1()
+print(a)
+fun2()
+print(a)
