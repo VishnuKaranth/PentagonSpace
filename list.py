638cd8b1326dd3b81aa6cf68d22359b3a89bb1cf
Vishnu <vishnukaranth04@gmail.com>
2025-04-02 17:47:57 +0530

    2nd April 2025

---
+++ list.py
@@ -0,0 +1,12 @@
+# This is a simple program to demonstrate the use of lists in Python
+# It allows the user to input 5 numbers and stores them in a list
+# Finally, it prints the numbers in the list
+arr = []
+i = 0
+while i < 5:
+    val = int(input("Enter a number: "))
+    arr.insert(i, val)
+    i += 1
+
+for num in arr:
+    print(num)
