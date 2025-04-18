9d140c49a9734243f965615e616918a1d48e0fd5
Vishnu <vishnukaranth04@gmail.com>
2025-04-18 17:11:51 +0530

    18th April 2025

---
+++ impmodule.py
@@ -0,0 +1,6 @@
+import module1
+import module2
+module1.fun1()
+module1.fun2()
+module2.fun1()
+module2.fun2()
---
+++ module2.py
@@ -0,0 +1,4 @@
+def fun1():
+    print("Inside fun1 p2")
+def fun2():
+    print("Inside fun2 p2")
