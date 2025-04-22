6eb08fb797b8b65fdb3cd69bf8a946f0a3a39f8a
Vishnu <vishnukaranth04@gmail.com>
2025-04-22 15:29:55 +0530

    22nd April 2025, today recreated the virtual env

---
+++ Python Programming/printonetofourwoloops.py
@@ -0,0 +1,16 @@
+def dispnum1(n):
+    print(n)
+    dispnum2(n+1)
+
+def dispnum2(n):
+    print(n)
+    dispnum3(n+1)
+
+def dispnum3(n):
+    print(n)
+    dispnum4(n+1)
+    
+def dispnum4(n):
+    print(n)
+
+dispnum1(1)
